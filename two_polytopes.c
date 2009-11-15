c43cdb622a6640f89beef021f3e71c90556b67a2
goering <devnull@localhost>
2009-11-15 19:39:46 +0100

    bug: max_p/q muss immer beides neu berechnet werden. nicht nur dort, wo sich was aendert.

--- two_polytopes.c
+++ two_polytopes.c
@@ -26,92 +26,6 @@ int compute_max_index(const struct svm_problem prob)
     return max_index;
 }
 
-void init_nodes(struct svm_node* vector, int elements)
-{
-    int i;
-    for (i=0;i< elements-1 ; i++)
-        vector[i].index = i+1;
-    vector[elements-1].index = -1;
-}
-
-void compute_vector_from_weights(double* weights, struct svm_node* vector, const struct svm_problem prob)
-{
-    int i;
-    for (i=0;i<prob.l;i++)
-    {
-        struct svm_node* out= vector;
-        struct svm_node* in = prob.x[0];
-        while (in->index != -1 && out->index != -1)
-        {
-            if (in->index == out->index)
-            {
-                if (i==0) // im ersten schritt den vektor auf null setzen
-                    out->value = 0.0; //todo: schoener machen
-                //printf("gleiche Indizes: %d  %d  , value = %f, weight = %f, result = %f outvalue = %f\n", in->index, out->index, in->value, weights[i], in->value*weights[0], out->value);
-                out->value += in->value * weights[i];
-                //printf("geschriebener Wert = %f \n", out->value);
-
-                ++in;
-                ++out;
-            }
-            else
-            {
-                //printf("missing element: %d  %d  \n", in->index, out->index);
-                if (in->index > out->index) {
-                    out->value = 0.0;
-                    ++out;
-                }
-                else {
-                    ++in;
-                }
-            }
-        }
-    }
-}
-
-double compute_dot_product_with_differences4(const struct svm_node* a, const struct svm_node* b, const struct svm_node* c, const struct svm_node* d) // <a - b, c - d>
-{
-//    printf(" start of function.. \n");
-//    printf(" a=%d  b=%d  c=%d  d=%d \n", a, b, c, d);
-/*    printf(" a=%d\n", a->index);
-    printf(" b=%d\n", b->index);
-    printf(" c=%d\n", c->index);
-    printf(" d=%d\n", d->index);*/
-//    printf(" a=%d  b=%d  c=%d  d=%d \n", a->index, b->index, c->index, d->index);
-    double sum = 0;
-    while (a->index != -1 && b->index != -1 && c->index != -1 && d->index != -1)
-    {
-        if ((a->index == b->index) && (c->index == d->index) &&  (a->index == d->index))
-        {
-            //printf("sum = %f  a = %f  b = %f  c = %f  d = %f\n", sum, a->value, b->value, c->value, d->value);
-            sum += (a->value - b->value)*(c->value - d->value);
-            ++a;
-            ++b;
-            ++c;
-            ++d;
-        }
-        else
-        {
-            //printf("missing element 2.. a=%d b=%d c=%d e=%d\n", a->index, b->index, c->index, d->index); // todo: was tun bei fehlenden werten?
-
-            if (a->index < b->index || a->index < c->index || a->index < d->index)
-                ++a;
-
-            if (b->index < a->index || b->index < c->index || b->index < d->index)
-                ++b;
-
-            if (c->index < a->index || c->index < b->index || c->index < d->index)
-                ++c;
-
-            if (d->index < a->index || d->index < b->index || d->index < c->index)
-                ++d;
-            //printf("missing element 3.. a=%d b=%d c=%d e=%d\n", a->index, b->index, c->index, d->index); // todo: was tun bei fehlenden werten?
-        }
-    }
-    return sum;
-}
-
-
 void print_vector( const struct svm_node* vector)
 {
     while (vector->index != -1)
@@ -146,32 +60,6 @@ double dot(const struct svm_node *px, const struct svm_node *py)
 	}
 	return sum;
 }
-
-double dotd(const struct svm_node *px, const struct svm_node *py)
-{
-    print_vector(px);
-    print_vector(py);
-	double sum = 0;
-	while(px->index != -1 && py->index != -1)
-	{
-		if(px->index == py->index)
-		{
-			sum += px->value * py->value;
-		    printf("  dot: %f = %f * %f, sum = %f, index = %d \n", px->value * py->value, px->value, py->value, sum, px->index);
-			++px;
-			++py;
-		}
-		else
-		{
-			if(px->index > py->index)
-				++py;
-			else
-				++px;
-		}
-	}
-	return sum;
-}
-
 
 int find_max_dotproduct_weights(double* weights_x, double* weights_y, struct svm_problem prob_x, struct svm_problem prob_y, double *max_ret)
 {
@@ -212,71 +100,15 @@ int find_max_dotproduct_weights(double* weights_x, double* weights_y, struct svm
     return max_index;
 }
 
-int find_max_dotproduct(const struct svm_node* x, const struct svm_node* y, struct svm_problem prob, double *max_ret)
-{
-//    printf("FIND MAX DOT PRODUCT\n");
-    int i;
-    double max = -HUGE_VAL; //compute_dot_product_with_differences(prob.x[0], x, y);
-    int max_index = -1;
-    for (i=0;i<prob.l;i++)
-//	for(i=0;i<10;i++)
-//	i=8;
-    {
-        //print_vector(x);
-        double dotproduct = compute_dot_product_with_differences4(prob.x[i], x, y, x);
-        if (dotproduct > max)
-        {
-            max = dotproduct;
-            max_index = i;
-            //printf("index = %d dotproduct = %f  max = %e \n", i, dotproduct, max);
-        }
-//        printf("index = %d dotproduct = %f  max = %e \n", i, dotproduct, max);
-        //print_vector(prob.x[0]);
-        //print_vector(x);
-        //print_vector(y);
-        //print_vector(x);
-    }
-    (*max_ret) = max; //todo: watch closely
-    return max_index;
-}
-
-
-void add_proportional(struct svm_node* a, const struct svm_node* b, double lambda)
-{
-    while (a->index != -1 && b->index != -1)
-    {
-        if (a->index == b->index)
-        {
-            //printf("a = %f  b = %f  lambda = %f \n", a->value, b->value, lambda);
-            a->value = lambda*a->value + (1.0 - lambda)* b->value;
-            //printf("a = %f  \n", a->value);
-            ++a;
-            ++b;
-        }
-        else
-        {
-        	//printf("skip: %d  %d \n", a->index, b->index);
-            if (a->index > b->index)
-                ++b;
-            else
-            {
-                ++a;
-            }
-        }
-    }
-}
-
 void add_to_weights(double* weights, double lambda, int index, struct svm_problem prob)
 {
     int i;
     for (i=0;i<prob.l;i++)
     {
-    	//printf("index = %d     lambda = %f     old=%f   ", i, lambda, weights[i]);
         if (i!=index)
             weights[i] *= lambda;
         else
             weights[i] = weights[i]*lambda + (1.0 - lambda)*1;
-        //printf("    new=%f  \n", weights[i]);
     }
 }
 
@@ -285,7 +117,6 @@ void print_weights(double* weights, struct svm_problem prob)
 	int i;
 	for(i=0;i<prob.l;i++)
 	{
-//		if(weights[i] != 0.0)
 			printf("%d:\t%f  ",i,weights[i]);
 	}
 	printf("\n");
@@ -294,7 +125,6 @@ void print_weights(double* weights, struct svm_problem prob)
 double compute_zaehler(double* x_weights, double* y_weights, const struct svm_node* x,  struct svm_problem prob_y, struct svm_problem prob_x)
 {
     double sum = 0.0;
-    double old_sum = 0.0;
     int j, k;
     for(j=0;j<prob_x.l;j++){
         for(k=0;k<prob_y.l;k++)
@@ -302,24 +132,17 @@ double compute_zaehler(double* x_weights, double* y_weights, const struct svm_no
             sum += x_weights[j] * y_weights[k] * dot(prob_x.x[j], prob_y.x[k]);
         }
     }
-//    printf("1 sum = %f  ", sum);
-//    old_sum = sum;
 
     for(j=0;j<prob_x.l;j++)
     {
         sum -= x_weights[j] * dot(prob_x.x[j], x);
-//        printf(" sum = %f, x_weights[j] = %f j = %d \n", sum, x_weights[j], j);
-//        print_vector(prob_x.x[j]);
     }
-//    printf("2 sum = %f  ", sum - old_sum);
-//    old_sum = sum;
+
     for(k=0;k<prob_y.l;k++) {
         sum -= y_weights[k] * dot(prob_y.x[k], x);
     }
-//    printf("3 sum = %f  ", sum - old_sum);
-//    old_sum = sum;
+
     sum += dot(x,x);
-//    printf("4sum = %f  \n", sum - old_sum);
     return sum;
 }
 
@@ -327,7 +150,6 @@ double compute_zaehler(double* x_weights, double* y_weights, const struct svm_no
 double compute_nenner(double* y_weights, const struct svm_node* x,  struct svm_problem prob_y)
 {
     double sum = 0.0;
-    double old_sum = 0.0;
     int j, k;
     for(j=0;j<prob_y.l;j++){
         for(k=0;k<prob_y.l;k++)
@@ -335,19 +157,41 @@ double compute_nenner(double* y_weights, const struct svm_node* x,  struct svm_p
             sum += y_weights[j] * y_weights[k] * dot(prob_y.x[j], prob_y.x[k]);
         }
     }
-//    printf("1 sum = %f  ", sum);
-//    old_sum = sum;
 
     for(j=0;j<prob_y.l;j++)
     {
         sum -= 2 * y_weights[j] * dot(prob_y.x[j], x);
-//        printf(" sum = %f, x_weights[j] = %f j = %d \n", sum, x_weights[j], j);
-//        print_vector(prob_x.x[j]);
     }
-//    printf("3 sum = %f  ", sum - old_sum);
-//    old_sum = sum;
+
     sum += dot(x,x);
-//    printf("4sum = %f  \n", sum - old_sum);
+    return sum;
+}
+
+double compute_distance(double* y_weights, double* x_weights, struct svm_problem prob_y, struct svm_problem prob_x)
+{
+    double sum = 0.0;
+    int j, k;
+    for(j=0;j<prob_x.l;j++){
+        for(k=0;k<prob_y.l;k++)
+        {
+            sum -= 2 * x_weights[j] * y_weights[k] * dot(prob_x.x[j], prob_y.x[k]);
+        }
+    }
+
+    for(j=0;j<prob_x.l;j++){
+        for(k=0;k<prob_x.l;k++)
+        {
+            sum += x_weights[j] * x_weights[k] * dot(prob_x.x[j], prob_x.x[k]);
+        }
+    }
+
+    for(j=0;j<prob_y.l;j++){
+        for(k=0;k<prob_y.l;k++)
+        {
+            sum += y_weights[j] * y_weights[k] * dot(prob_y.x[j], prob_y.x[k]);
+        }
+    }
+
     return sum;
 }
 
@@ -389,12 +233,8 @@ int main (int argc, const char ** argv)
 
     printf("Gewichtsvektoren initialisieren.. \n");
     double* x_weights = (double *) malloc(prob_p.l * sizeof(double));
-    struct svm_node* x = (struct svm_node *) malloc(elements * sizeof(struct svm_node));
-    init_nodes(x, elements);
 
     double* y_weights = (double *) malloc(prob_q.l * sizeof(double));
-    struct svm_node* y = (struct svm_node *) malloc(elements * sizeof(struct svm_node));
-    init_nodes(y, elements);
 
     int i;
     x_weights[0] = 1.0;
@@ -405,151 +245,79 @@ int main (int argc, const char ** argv)
     for (i=1;i<prob_q.l;i++)
         y_weights[i]=0.0;
 
-
-
-    printf("Vektoren aus den Gewichtsvektoren berechnen.. \n");
-    compute_vector_from_weights(x_weights, x, prob_p);
-    compute_vector_from_weights(y_weights, y, prob_q);
-
-//    print_vector(x);
-//    print_weights(x_weights, prob_p);
-//    print_vector(y);
-//    print_weights(y_weights, prob_q);
-
-
     // Step 1
     printf("Maximales Kreuzprodukt suchen... \n");
     double max_p;
-    int max_p_index = find_max_dotproduct( x, y, prob_p, &max_p);
-
     double max_q;
-    int max_q_index = find_max_dotproduct( y, x, prob_q, &max_q);
 
-
-
-    double max_p_weights;
-    int max_p_index_weights = find_max_dotproduct_weights( x_weights, y_weights, prob_p, prob_q, &max_p_weights);
-
-    double max_q_weights;
-    int max_q_index_weights = find_max_dotproduct_weights( y_weights, x_weights, prob_q, prob_p, &max_q_weights);
+    int max_p_index = find_max_dotproduct_weights( x_weights, y_weights, prob_p, prob_q, &max_p);
+    int max_q_index = find_max_dotproduct_weights( y_weights, x_weights, prob_q, prob_p, &max_q);
 
 
     int j;
 
     for (j=0;j<3;j++)
     {
-    //printf("max_q = %f  %f, index = %d %d    max_p = %f  %f, index = %d %d \n", max_q, max_q_weights, max_q_index, max_q_index_weights,
-    //max_p, max_p_weights, max_p_index, max_p_index_weights);
-//        printf(" ... P = %d (%f), Q = %d (%f)\n", max_p_index, max_p, max_q_index, max_q);
         double lambda;
-            //printf(" max_q_index = %d , max_p_index = %d \n", max_q_index, max_p_index);
-
-        if (max_p > max_q)
+        if (max_p >= max_q)
         {
-//            printf("  max_p > max_q \n");
-            /*double zaehler = compute_dot_product_with_differences4(y, prob_p.x[max_p_index], x, prob_p.x[max_p_index]);
-            double nenner = compute_dot_product_with_differences4(x, prob_p.x[max_p_index], x, prob_p.x[max_p_index]);
-
-//            printf("zaehler=%f  nenner=%f \n", zaehler, nenner);
-
-            printf("1zaehler = %f  %f \n", zaehler, compute_zaehler(y_weights, x_weights, prob_p.x[max_p_index], prob_p, prob_q) );
-            printf("1nenner = %f  %f \n", nenner, compute_nenner(x_weights, prob_p.x[max_p_index], prob_p) );
-*/
             double zaehler = compute_zaehler(y_weights, x_weights, prob_p.x[max_p_index], prob_p, prob_q);
             double nenner = compute_nenner(x_weights, prob_p.x[max_p_index], prob_p);
 
             lambda = zaehler / nenner;
 
+            if(zaehler == 0.0 && nenner == 0.0) lambda = 0.0;
             if(lambda < 0.0)	lambda = 0.0;
-
-			//print_weights(x_weights,prob_p);
+            if(lambda > 1.0)	lambda = 0.0;
 
             printf("lambda = %f  zaehler = %f  nenner = %f \n", lambda, zaehler, nenner);
 
-            add_proportional(x,prob_p.x[max_p_index], lambda);
+
             add_to_weights(x_weights, lambda, max_p_index, prob_p);
 
-//    print_vector(x);
-//    print_weights(x_weights, prob_p);
-//    print_vector(y);
-//    print_weights(y_weights, prob_q);
-			//print_weights(x_weights,prob_p);
-
-
-            max_p_index = find_max_dotproduct( x, y, prob_p, &max_p); // max_p updaten
-            max_p_index_weights = find_max_dotproduct_weights( x_weights, y_weights, prob_p, prob_q, &max_p_weights); // max_p updaten
+            max_p_index = find_max_dotproduct_weights( x_weights, y_weights, prob_p, prob_q, &max_p); // max_p updaten
+            max_q_index = find_max_dotproduct_weights( y_weights, x_weights, prob_q, prob_p, &max_q); // max_p updaten
 
         }
         else
         {
-//            printf("  max_p <= max_q \n");
-            // Gilbertschritt im Polytop Q
-            // \lambda = <p-q_i, q-q_i> / <q-q_i, q-q_i>
-
-            // <x - max_q_index, y - max_q_index> /  <y - max_q_index, y - max_q_index>
-
-
-            /*double zaehler = compute_dot_product_with_differences4(x, prob_q.x[max_q_index], y, prob_q.x[max_q_index]);
-            double nenner = compute_dot_product_with_differences4(y, prob_q.x[max_q_index], y, prob_q.x[max_q_index]);
-
-
-            printf("2zaehler = %f  %f \n", zaehler, compute_zaehler(x_weights, y_weights, prob_q.x[max_q_index], prob_q, prob_p) );
-            printf("2nenner = %f  %f \n", nenner, compute_nenner(y_weights, prob_q.x[max_q_index], prob_q) );
-*/
 
             double zaehler = compute_zaehler(x_weights, y_weights, prob_q.x[max_q_index], prob_q, prob_p);
             double nenner = compute_nenner(y_weights, prob_q.x[max_q_index], prob_q);
-
-//            printf("zaehler=%f  nenner=%f \n", zaehler, nenner);
-
-//            print_vector(x);
-//            print_weights(x_weights, prob_p);
-//            print_vector(y);
-//            print_weights(y_weights, prob_q);
-//            print_vector(prob_q.x[max_q_index]);
 
             lambda = zaehler / nenner;
 
             if(zaehler == 0.0 && nenner == 0.0) lambda = 0.0;
             if(lambda < 0.0)	lambda = 0.0;
+            if(lambda > 1.0)	lambda = 0.0;
 
-            //printf("lambda = %e  zaehler = %e  nenner = %e \n", lambda, zaehler, nenner);
-            add_proportional(y,prob_q.x[max_q_index], lambda);
             add_to_weights(y_weights, lambda, max_q_index, prob_q);
 
-/*    print_vector(x);
-    print_weights(x_weights, prob_p);
-    print_vector(y);
-    print_weights(y_weights, prob_q);
-*/
-            max_q_index = find_max_dotproduct( y, x, prob_q, &max_q); // max_q updaten
-            max_q_index_weights = find_max_dotproduct_weights( y_weights, x_weights, prob_q, prob_p, &max_q_weights); // max_p updaten
-        }
+            max_q_index = find_max_dotproduct_weights( y_weights, x_weights, prob_q, prob_p, &max_q); // max_p updaten
+            max_p_index = find_max_dotproduct_weights( x_weights, y_weights, prob_p, prob_q, &max_p); // max_p updaten
+       }
 
         //duality gap
         // absolute duality gap
-        // berechne max_p und max_q neu, dann adg = max_p+max_q
 
         double adg = max_p + max_q;
 
-        printf("absolute duality gap = %e \n", adg);
+        printf("max_p = %f  max_q = %f ", max_p, max_q);
+        printf("adg = %f ", adg);
 
         // relative duality gap
         // adg / ||p-q||^2 - adg
         // adg / <p-q, p-q> - adg
 
-		printf("<x-y,x-y> = %e\n" , compute_dot_product_with_differences4(x,y,x,y));
-
-		//print_vector(x);
-		//print_vector(y);
+		printf("<x-y,x-y> = %f " , compute_distance(x_weights,y_weights,prob_p,prob_q));
 
 
-        double rdg_nenner = compute_dot_product_with_differences4(x,y,x,y) - adg;
+        double rdg_nenner = compute_distance(x_weights,y_weights,prob_p,prob_q) - adg;
         double rdg;
 
         if (rdg_nenner <= 0)
         {
-            printf("set huge value... \n");
+            printf("set huge value... ");
             rdg = HUGE_VAL;
         }
         else
@@ -557,7 +325,9 @@ int main (int argc, const char ** argv)
             rdg = adg / rdg_nenner;
         }
 
-        printf("relative duality gap = %f \n", rdg);
+        printf("rdg = %f \n", rdg);
+		print_weights(x_weights, prob_p);
+		print_weights(y_weights, prob_q);
 
     }
 
