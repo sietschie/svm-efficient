bb6bddc6be149cb6af532d3684fb8d7b67f17c57
goering <devnull@localhost>
2009-11-24 13:38:22 +0100

    added efficient computations

--- two_polytopes.c
+++ two_polytopes.c
@@ -61,12 +61,16 @@ double dot(const struct svm_node *px, const struct svm_node *py)
 	return sum;
 }
 
-int find_max_dotproduct_weights(double* weights_x, double* weights_y, struct svm_problem prob_x, struct svm_problem prob_y, double *max_ret)
+int find_max_dotproduct_weights(double* weights_x,
+                                double* weights_y,
+                                struct svm_problem prob_x,
+                                struct svm_problem prob_y,
+                                double *max_ret)
 {
     int i;
     double max = -HUGE_VAL; //compute_dot_product_with_differences(prob.x[0], x, y);
     int max_index = -1;
-    for (i=0;i<prob.l;i++)
+    for (i=0;i<prob_x.l;i++) // wtf?
     {
         double sum = 0.0;
         int j;
@@ -90,7 +94,6 @@ int find_max_dotproduct_weights(double* weights_x, double* weights_y, struct svm
                 sum += weights_x[j] * weights_x[k] * dot(prob_x.x[j], prob_x.x[k]);
             }
         }
-
         if( max < sum) {
             max = sum;
             max_index = i;
@@ -236,14 +239,76 @@ int main (int argc, const char ** argv)
 
     double* y_weights = (double *) malloc(prob_q.l * sizeof(double));
 
+    // initialize weights
     int i;
-    x_weights[0] = 1.0;
-    for (i=1;i<prob_p.l;i++)
-        x_weights[i]=0.0;
+    for (i=0;i<prob_p.l;i++)
+        x_weights[i] = 0.0;
 
+    for (i=0;i<prob_q.l;i++)
+        y_weights[i] = 0.0;
+
+    x_weights[0] = 1.0;
     y_weights[0] = 1.0;
-    for (i=1;i<prob_q.l;i++)
-        y_weights[i]=0.0;
+
+
+    // deklaration der variablen die werte zwischenspeichern
+    double *dot_xi_x; // < x_i, x> \forall x \in P
+    double *dot_yi_x;  // < y_i, x> \forall x \in P
+    double dot_xi_yi; // <x_i, y_i >
+    double dot_xi_xi; // <x_i, x_i >
+
+    double *dot_yi_y; // < y_i, y> \forall y \in Q
+    double *dot_xi_y;  // < x_i, y> \forall y \in Q
+    double dot_yi_yi; // <y_i, y_i >
+
+    // speicher anfordern
+    dot_xi_x = (double *) malloc(prob_p.l * sizeof(double));
+    dot_yi_x = (double *) malloc(prob_p.l * sizeof(double));
+
+    dot_xi_y = (double *) malloc(prob_q.l * sizeof(double));
+    dot_yi_y = (double *) malloc(prob_q.l * sizeof(double));
+
+    // initialisieren
+    for (i=0;i<prob_p.l;i++) {
+        dot_xi_x[i]=dot(prob_p.x[0], prob_p.x[i]);
+        dot_yi_x[i]=dot(prob_q.x[0], prob_p.x[i]);
+    }
+
+    for (i=0;i<prob_q.l;i++) {
+        dot_xi_y[i]=dot(prob_p.x[0], prob_q.x[i]);
+        dot_yi_y[i]=dot(prob_q.x[0], prob_q.x[i]);
+    }
+
+    dot_xi_xi = dot(prob_p.x[0], prob_p.x[0]);
+    dot_xi_yi = dot(prob_p.x[0], prob_q.x[0]);
+    dot_yi_yi = dot(prob_q.x[0], prob_q.x[0]);
+
+
+    // find max
+    int max_p_index_e = -1;
+    double max_p_e = -HUGE_VAL;
+    for (i=0;i<prob_p.l;i++) {
+        double sum = dot_yi_x[i] - dot_xi_x[i] - dot_xi_yi + dot_xi_xi;
+
+        if(sum > max_p_e)
+        {
+            max_p_e = sum;
+            max_p_index_e = i;
+        }
+    }
+
+    int max_q_index_e = -1;
+    double max_q_e = -HUGE_VAL;
+    for (i=0;i<prob_q.l;i++) {
+        double sum = dot_xi_y[i] - dot_yi_y[i] - dot_xi_yi + dot_yi_yi;
+        if(sum > max_q_e)
+        {
+            max_q_e = sum;
+            max_q_index_e = i;
+        }
+    }
+
+
 
     // Step 1
     printf("Maximales Kreuzprodukt suchen... \n");
@@ -253,10 +318,9 @@ int main (int argc, const char ** argv)
     int max_p_index = find_max_dotproduct_weights( x_weights, y_weights, prob_p, prob_q, &max_p);
     int max_q_index = find_max_dotproduct_weights( y_weights, x_weights, prob_q, prob_p, &max_q);
 
-
     int j;
 
-    for (j=0;j<3;j++)
+    for (j=0;j<10;j++)
     {
         double lambda;
         if (max_p >= max_q)
@@ -264,20 +328,60 @@ int main (int argc, const char ** argv)
             double zaehler = compute_zaehler(y_weights, x_weights, prob_p.x[max_p_index], prob_p, prob_q);
             double nenner = compute_nenner(x_weights, prob_p.x[max_p_index], prob_p);
 
+            double zaehler_e = dot_xi_yi - dot_yi_x[max_p_index] - dot_xi_x[max_p_index] + dot(prob_p.x[max_p_index], prob_p.x[max_p_index]);
+            double nenner_e = dot_xi_xi - 2* dot_xi_x[max_p_index] +  dot(prob_p.x[max_p_index], prob_p.x[max_p_index]);
+
             lambda = zaehler / nenner;
 
             if(zaehler == 0.0 && nenner == 0.0) lambda = 0.0;
             if(lambda < 0.0)	lambda = 0.0;
             if(lambda > 1.0)	lambda = 0.0;
 
-            printf("lambda = %f  zaehler = %f  nenner = %f \n", lambda, zaehler, nenner);
+            // update dotproducts
+
+            dot_xi_xi = lambda * lambda * dot_xi_xi
+                        + 2 * lambda * (1.0 - lambda) * dot_xi_x[max_p_index]
+                        + (1.0 - lambda)*(1.0 - lambda)*dot(prob_p.x[max_p_index],prob_p.x[max_p_index] );
+
+            dot_xi_yi = lambda * dot_xi_yi + (1.0 - lambda) * dot_yi_x[max_p_index];
+
+            for (i=0;i<prob_p.l;i++) {
+                dot_xi_x[i]= dot_xi_x[i] * lambda + (1.0 - lambda) * dot(prob_p.x[max_p_index], prob_p.x[i]);
+            }
+
+            for (i=0;i<prob_q.l;i++) {
+                dot_xi_y[i]= dot_xi_y[i] * lambda + (1.0 - lambda) * dot(prob_p.x[max_p_index], prob_q.x[i]);
+            }
+
+            // find max
+            int max_p_index_e = -1;
+            double max_p_e = -HUGE_VAL;
+            for (i=0;i<prob_p.l;i++) {
+                double sum = dot_yi_x[i] - dot_xi_x[i] - dot_xi_yi + dot_xi_xi;
+                if(sum > max_p_e)
+                {
+                    max_p_e = sum;
+                    max_p_index_e = i;
+                }
+            }
+
+            int max_q_index_e = -1;
+            double max_q_e = -HUGE_VAL;
+            for (i=0;i<prob_q.l;i++) {
+                double sum = dot_xi_y[i] - dot_yi_y[i] - dot_xi_yi + dot_yi_yi;
+                if(sum > max_q_e)
+                {
+                    max_q_e = sum;
+                    max_q_index_e = i;
+                }
+            }
+
 
 
             add_to_weights(x_weights, lambda, max_p_index, prob_p);
 
             max_p_index = find_max_dotproduct_weights( x_weights, y_weights, prob_p, prob_q, &max_p); // max_p updaten
             max_q_index = find_max_dotproduct_weights( y_weights, x_weights, prob_q, prob_p, &max_q); // max_p updaten
-
         }
         else
         {
@@ -285,14 +389,59 @@ int main (int argc, const char ** argv)
             double zaehler = compute_zaehler(x_weights, y_weights, prob_q.x[max_q_index], prob_q, prob_p);
             double nenner = compute_nenner(y_weights, prob_q.x[max_q_index], prob_q);
 
+            double zaehler_e = dot_xi_yi - dot_xi_y[max_q_index] - dot_yi_y[max_q_index] + dot(prob_q.x[max_q_index], prob_q.x[max_q_index]);
+            double nenner_e = dot_yi_yi - 2* dot_yi_y[max_q_index] +  dot(prob_q.x[max_q_index], prob_q.x[max_q_index]);
+
             lambda = zaehler / nenner;
 
             if(zaehler == 0.0 && nenner == 0.0) lambda = 0.0;
             if(lambda < 0.0)	lambda = 0.0;
             if(lambda > 1.0)	lambda = 0.0;
 
-            add_to_weights(y_weights, lambda, max_q_index, prob_q);
+            // update dotproducts
 
+            dot_yi_yi = lambda * lambda * dot_yi_yi
+                        + 2 * lambda * (1.0 - lambda) * dot_yi_y[max_q_index]
+                        + (1.0 - lambda)*(1.0 - lambda)*dot(prob_q.x[max_q_index],prob_q.x[max_q_index] );
+
+            dot_xi_yi = lambda * dot_xi_yi + (1.0 - lambda) * dot_xi_y[max_q_index];
+
+            for (i=0;i<prob_q.l;i++) {
+                dot_yi_y[i]= dot_yi_y[i] * lambda + (1.0 - lambda) * dot(prob_q.x[max_q_index], prob_q.x[i]);
+            }
+
+            for (i=0;i<prob_p.l;i++) {
+                dot_yi_x[i]= dot_yi_x[i] * lambda + (1.0 - lambda) * dot(prob_q.x[max_q_index], prob_p.x[i]);
+            }
+
+            // find max
+            int max_p_index_e = -1;
+            double max_p_e = -HUGE_VAL;
+            for (i=0;i<prob_p.l;i++) {
+                double sum = dot_yi_x[i] - dot_xi_x[i] - dot_xi_yi + dot_xi_xi;
+                if(sum > max_p_e)
+                {
+                    max_p_e = sum;
+                    max_p_index_e = i;
+                }
+            }
+
+            int max_q_index_e = -1;
+            double max_q_e = -HUGE_VAL;
+            for (i=0;i<prob_q.l;i++) {
+                double sum = dot_xi_y[i] - dot_yi_y[i] - dot_xi_yi + dot_yi_yi;
+                if(sum > max_q_e)
+                {
+                    max_q_e = sum;
+                    max_q_index_e = i;
+                }
+            }
+
+
+
+
+
+            add_to_weights(y_weights, lambda, max_q_index, prob_q);
             max_q_index = find_max_dotproduct_weights( y_weights, x_weights, prob_q, prob_p, &max_q); // max_p updaten
             max_p_index = find_max_dotproduct_weights( x_weights, y_weights, prob_p, prob_q, &max_p); // max_p updaten
        }
