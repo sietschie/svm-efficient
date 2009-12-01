637614e59d561718f95e42ec859ca42f4cbf8f24
goering <devnull@localhost>
2009-12-01 15:15:24 +0100

    wechsel von p/q zu indizes fuer mengenvariablen

--- two_polytopes.c
+++ two_polytopes.c
@@ -6,9 +6,6 @@
 //#define INFINITY	__builtin_inf() // todo: get rid of that
 
 
-struct svm_problem prob_p;
-struct svm_problem prob_q;
-
 int compute_max_index(const struct svm_problem prob)
 {
     int i;
@@ -61,10 +58,16 @@ double dot(const struct svm_node *px, const struct svm_node *py)
 	return sum;
 }
 
-void add_to_weights(double* weights, double lambda, int index, struct svm_problem prob)
+double kernel(int set1, int e1ement1, int set2, int element2)
+{
+    return dot(prob[set1].x[e1ement1], prob[set2].x[element2]);
+}
+
+
+void add_to_weights(double* weights, double lambda, int index, int set)
 {
     int i;
-    for (i=0;i<prob.l;i++)
+    for (i=0;i<prob[set].l;i++)
     {
         if (i!=index)
             weights[i] *= lambda;
@@ -73,22 +76,22 @@ void add_to_weights(double* weights, double lambda, int index, struct svm_proble
     }
 }
 
-void print_weights(double* weights, struct svm_problem prob)
+void print_weights(double* weights, int set)
 {
 	int i;
-	for(i=0;i<prob.l;i++)
+	for(i=0;i<prob[set].l;i++)
 	{
 			printf("%d:\t%f  ",i,weights[i]);
 	}
 	printf("\n");
 }
 
-int find_max(struct svm_problem prob_p, double *dot_yi_x, double* dot_xi_x, double dot_xi_yi, double dot_xi_xi, double *max_p) {
+int find_max(int p, double *dot_yi_x, double* dot_xi_x, double dot_xi_yi, double dot_xi_xi, double *max_p) {
     // find max
     int max_p_index = -1;
     *max_p = -HUGE_VAL;
     int i;
-    for (i=0;i<prob_p.l;i++) {
+    for (i=0;i<prob[p].l;i++) {
         double sum = dot_yi_x[i] - dot_xi_x[i] - dot_xi_yi + dot_xi_xi;
         if(sum > *max_p)
         {
@@ -99,20 +102,20 @@ int find_max(struct svm_problem prob_p, double *dot_yi_x, double* dot_xi_x, doub
     return max_p_index;
 }
 
-double compute_zaehler(double dot_xi_yi, double* dot_yi_x, double* dot_xi_x, struct svm_problem prob_p, int max_p_index ) {
-    double zaehler = dot_xi_yi - dot_yi_x[max_p_index] - dot_xi_x[max_p_index] + dot(prob_p.x[max_p_index], prob_p.x[max_p_index]);
+double compute_zaehler(double dot_xi_yi, double* dot_yi_x, double* dot_xi_x, int p, int max_p_index ) {
+    double zaehler = dot_xi_yi - dot_yi_x[max_p_index] - dot_xi_x[max_p_index] + kernel(p,max_p_index, p, max_p_index); //todo: samevector
     return zaehler;
 }
 
-double compute_nenner(double dot_xi_xi, double* dot_xi_x, struct svm_problem prob_p, int max_p_index) {
-    double nenner = dot_xi_xi - 2* dot_xi_x[max_p_index] +  dot(prob_p.x[max_p_index], prob_p.x[max_p_index]);
+double compute_nenner(double dot_xi_xi, double* dot_xi_x, int p, int max_p_index) {
+    double nenner = dot_xi_xi - 2* dot_xi_x[max_p_index] +  kernel(p, max_p_index, p, max_p_index);
     return nenner;
 }
 
-double update_xi_xi(double dot_xi_xi, double* dot_xi_x, struct svm_problem prob_p, int max_p_index, double lambda) {
+double update_xi_xi(double dot_xi_xi, double* dot_xi_x, int p, int max_p_index, double lambda) {
     dot_xi_xi = lambda * lambda * dot_xi_xi
             + 2 * lambda * (1.0 - lambda) * dot_xi_x[max_p_index]
-            + (1.0 - lambda)*(1.0 - lambda)*dot(prob_p.x[max_p_index],prob_p.x[max_p_index] );
+            + (1.0 - lambda)*(1.0 - lambda)*kernel(p, max_p_index, p ,max_p_index );
     return dot_xi_xi;
 }
 
@@ -121,10 +124,10 @@ double update_xi_yi(double dot_xi_yi, double* dot_yi_x, int max_p_index, double 
     return dot_xi_yi;
 }
 
-void update_xi_x(double* dot_xi_x, struct svm_problem prob_p, struct svm_problem prob_p2, int max_p_index, double lambda) {
+void update_xi_x(double* dot_xi_x, int p, int p2, int max_p_index, double lambda) {
     int i;
-    for (i=0;i<prob_p2.l;i++) {
-        dot_xi_x[i]= dot_xi_x[i] * lambda + (1.0 - lambda) * dot(prob_p.x[max_p_index], prob_p2.x[i]);
+    for (i=0;i<prob[p2].l;i++) {
+        dot_xi_x[i]= dot_xi_x[i] * lambda + (1.0 - lambda) * kernel(p, max_p_index, p2, i);
     }
 }
 
@@ -142,14 +145,10 @@ int main (int argc, const char ** argv)
 
     read_problem(filename);
 
-    prob_p = prob[0];
-    prob_q = prob[1];
+    printf("Anzahl der eingelesenen Datensaetze: P = %d   Q = %d \n", prob[0].l, prob[1].l);
 
-
-    printf("Anzahl der eingelesenen Datensaetze: P = %d   Q = %d \n", prob_p.l, prob_q.l);
-
-    int max_index_q = compute_max_index(prob_q); //Todo: besseren namen finden
-    int max_index_p = compute_max_index(prob_p);
+    int max_index_q = compute_max_index(prob[1]); //Todo: besseren namen finden
+    int max_index_p = compute_max_index(prob[0]);
 
     printf("Groesse der Vektoren: P = %d, Q = %d \n", max_index_p, max_index_q);
 
@@ -158,16 +157,16 @@ int main (int argc, const char ** argv)
     elements++; //add one for the stopping index -1
 
     printf("Gewichtsvektoren initialisieren.. \n");
-    double* x_weights = (double *) malloc(prob_p.l * sizeof(double));
+    double* x_weights = (double *) malloc(prob[0].l * sizeof(double));
 
-    double* y_weights = (double *) malloc(prob_q.l * sizeof(double));
+    double* y_weights = (double *) malloc(prob[1].l * sizeof(double));
 
     // initialize weights
     int i;
-    for (i=0;i<prob_p.l;i++)
+    for (i=0;i<prob[0].l;i++)
         x_weights[i] = 0.0;
 
-    for (i=0;i<prob_q.l;i++)
+    for (i=0;i<prob[1].l;i++)
         y_weights[i] = 0.0;
 
     x_weights[0] = 1.0;
@@ -185,36 +184,36 @@ int main (int argc, const char ** argv)
     double dot_yi_yi; // <y_i, y_i >
 
     // speicher anfordern
-    dot_xi_x = (double *) malloc(prob_p.l * sizeof(double));
-    dot_yi_x = (double *) malloc(prob_p.l * sizeof(double));
+    dot_xi_x = (double *) malloc(prob[0].l * sizeof(double));
+    dot_yi_x = (double *) malloc(prob[0].l * sizeof(double));
 
-    dot_xi_y = (double *) malloc(prob_q.l * sizeof(double));
-    dot_yi_y = (double *) malloc(prob_q.l * sizeof(double));
+    dot_xi_y = (double *) malloc(prob[1].l * sizeof(double));
+    dot_yi_y = (double *) malloc(prob[1].l * sizeof(double));
 
     // initialisieren
-    for (i=0;i<prob_p.l;i++) {
-        dot_xi_x[i]=dot(prob_p.x[0], prob_p.x[i]);
-        dot_yi_x[i]=dot(prob_q.x[0], prob_p.x[i]);
+    for (i=0;i<prob[0].l;i++) {
+        dot_xi_x[i]=kernel(0, 0, 0, i);
+        dot_yi_x[i]=kernel(1, 0, 0, i);
     }
 
-    for (i=0;i<prob_q.l;i++) {
-        dot_xi_y[i]=dot(prob_p.x[0], prob_q.x[i]);
-        dot_yi_y[i]=dot(prob_q.x[0], prob_q.x[i]);
+    for (i=0;i<prob[1].l;i++) {
+        dot_xi_y[i]=kernel(0, 0, 1, i);
+        dot_yi_y[i]=kernel(1, 0, 1, i);
     }
 
-    dot_xi_xi = dot(prob_p.x[0], prob_p.x[0]);
-    dot_xi_yi = dot(prob_p.x[0], prob_q.x[0]);
-    dot_yi_yi = dot(prob_q.x[0], prob_q.x[0]);
+    dot_xi_xi = kernel(0, 0, 0, 0);
+    dot_xi_yi = kernel(0, 0, 1, 0);
+    dot_yi_yi = kernel(1, 0, 1, 0);
 
 
     // find max
     int max_p_index;
     double max_p;
-    max_p_index = find_max(prob_p, dot_yi_x, dot_xi_x, dot_xi_yi, dot_xi_xi, &max_p);
+    max_p_index = find_max(0, dot_yi_x, dot_xi_x, dot_xi_yi, dot_xi_xi, &max_p);
 
     int max_q_index;
     double max_q;
-    max_q_index = find_max(prob_q, dot_xi_y, dot_yi_y, dot_xi_yi, dot_yi_yi, &max_q);
+    max_q_index = find_max(1, dot_xi_y, dot_yi_y, dot_xi_yi, dot_yi_yi, &max_q);
 
     int j;
 
@@ -223,8 +222,8 @@ int main (int argc, const char ** argv)
         double lambda;
         if (max_p >= max_q)
         {
-            double zaehler = compute_zaehler(dot_xi_yi, dot_yi_x, dot_xi_x, prob_p, max_p_index);
-            double nenner = compute_nenner(dot_xi_xi, dot_xi_x, prob_p, max_p_index);
+            double zaehler = compute_zaehler(dot_xi_yi, dot_yi_x, dot_xi_x, 0, max_p_index);
+            double nenner = compute_nenner(dot_xi_xi, dot_xi_x, 0, max_p_index);
 
             lambda = zaehler / nenner;
 
@@ -232,22 +231,22 @@ int main (int argc, const char ** argv)
             if(lambda < 0.0)	lambda = 0.0;
             if(lambda > 1.0)	lambda = 0.0;
 
-            add_to_weights(x_weights, lambda, max_p_index, prob_p);
+            add_to_weights(x_weights, lambda, max_p_index, 0);
 
             // update dotproducts
 
-            dot_xi_xi = update_xi_xi(dot_xi_xi, dot_xi_x, prob_p, max_p_index, lambda);
+            dot_xi_xi = update_xi_xi(dot_xi_xi, dot_xi_x, 0, max_p_index, lambda);
 
             dot_xi_yi = update_xi_yi(dot_xi_yi, dot_yi_x, max_p_index, lambda);
 
-            update_xi_x(dot_xi_x, prob_p, prob_p, max_p_index, lambda);
+            update_xi_x(dot_xi_x, 0, 0, max_p_index, lambda);
 
-            update_xi_x(dot_xi_y, prob_p, prob_q, max_p_index, lambda);
+            update_xi_x(dot_xi_y, 0, 1, max_p_index, lambda);
         }
         else
         {
-            double zaehler = compute_zaehler(dot_xi_yi, dot_xi_y, dot_yi_y, prob_q, max_q_index);
-            double nenner = compute_nenner(dot_yi_yi, dot_yi_y, prob_q, max_q_index);
+            double zaehler = compute_zaehler(dot_xi_yi, dot_xi_y, dot_yi_y, 1, max_q_index);
+            double nenner = compute_nenner(dot_yi_yi, dot_yi_y, 1, max_q_index);
 
             lambda = zaehler / nenner;
 
@@ -255,21 +254,21 @@ int main (int argc, const char ** argv)
             if(lambda < 0.0)	lambda = 0.0;
             if(lambda > 1.0)	lambda = 0.0;
 
-            add_to_weights(y_weights, lambda, max_q_index, prob_q);
+            add_to_weights(y_weights, lambda, max_q_index, 1);
 
             // update dotproducts
 
-            dot_yi_yi = update_xi_xi(dot_yi_yi, dot_yi_y, prob_q, max_q_index, lambda);
+            dot_yi_yi = update_xi_xi(dot_yi_yi, dot_yi_y, 1, max_q_index, lambda);
 
             dot_xi_yi = update_xi_yi(dot_xi_yi, dot_xi_y, max_q_index, lambda);
 
-            update_xi_x(dot_yi_y, prob_q, prob_q, max_q_index, lambda);
+            update_xi_x(dot_yi_y, 1, 1, max_q_index, lambda);
 
-            update_xi_x(dot_yi_x, prob_q, prob_p, max_q_index, lambda);
+            update_xi_x(dot_yi_x, 1, 0, max_q_index, lambda);
         }
         // find max
-        max_q_index = find_max(prob_q, dot_xi_y, dot_yi_y, dot_xi_yi, dot_yi_yi, &max_q);
-        max_p_index = find_max(prob_p, dot_yi_x, dot_xi_x, dot_xi_yi, dot_xi_xi, &max_p);
+        max_q_index = find_max(1, dot_xi_y, dot_yi_y, dot_xi_yi, dot_yi_yi, &max_q);
+        max_p_index = find_max(0, dot_yi_x, dot_xi_x, dot_xi_yi, dot_xi_xi, &max_p);
 
 
         //duality gap
@@ -303,8 +302,8 @@ int main (int argc, const char ** argv)
         }
 
         printf("rdg = %e \n", rdg);
-		//print_weights(x_weights, prob_p);
-		//print_weights(y_weights, prob_q);
+		//print_weights(x_weights, prob[0]);
+		//print_weights(y_weights, prob[1]);
 
     }
 
