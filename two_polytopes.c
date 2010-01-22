a509e7f0b089c60a43a4783d28f81d016c3be8b1
goering <devnull@localhost>
2010-01-22 13:41:34 +0100

    cache added

--- two_polytopes.c
+++ two_polytopes.c
@@ -6,6 +6,8 @@
 #include "readsvm.h"
 //#define INFINITY	__builtin_inf() // todo: get rid of that
 
+double* get_element(int id, int set);
+void init(int noce, int noe);
 
 
 struct svm_parameter param;		// set by parse_command_line
@@ -230,7 +232,11 @@ double dot(const struct svm_node *px, const struct svm_node *py)
 double kernel_linear(int set1, int element1, int set2, int element2)
 //double kernel(int set1, int element1, int set2, int element2)
 {
-    return dot(prob[set1].x[element1], prob[set2].x[element2]);
+    double ret = dot(prob[set1].x[element1], prob[set2].x[element2]);
+    if(set1 == set2 && element1 == element2)
+//        ret += param.C;
+        ret += 0;
+    return ret;
 }
 
 double kernel_poly(int set1, int element1, int set2, int element2)
@@ -330,10 +336,17 @@ double update_xi_yi(double dot_xi_yi, double* dot_yi_x, int max_p_index, double 
 }
 
 void update_xi_x(double* dot_xi_x, int p, int p2, int max_p_index, double lambda) {
+    printf("update_xi_x(): %d %d %d \n", p, p2, max_p_index);
+    double* computed_kernels = get_element(max_p_index, p);
+
     int i;
     for (i=0;i<prob[p2].l;i++) {
-        dot_xi_x[i]= dot_xi_x[i] * lambda + (1.0 - lambda) * kernel(p, max_p_index, p2, i);
+        //dot_xi_x[i]= dot_xi_x[i] * lambda + (1.0 - lambda) * kernel(p, max_p_index, p2, i);
+        int offset = p2 * prob[0].l;
+        dot_xi_x[i]= dot_xi_x[i] * lambda + (1.0 - lambda) * computed_kernels[ offset + i  ]; //(p, max_p_index, p2, i);
+        printf(" %f ", computed_kernels[ offset + i  ]);
     }
+    printf("\n");
 }
 
 void compute_weights(double *x_weights, double* y_weights)
@@ -397,7 +410,7 @@ void compute_weights(double *x_weights, double* y_weights)
 
     int j;
 
-    for (j=0;j<10  ;j++)
+    for (j=0;j<2  ;j++)
     {
         double lambda;
         if (max_p >= max_q)
@@ -537,6 +550,9 @@ int main (int argc, char ** argv)
 
     printf("Anzahl der eingelesenen Datensaetze: P = %d   Q = %d \n", prob[0].l, prob[1].l);
 
+    // init cache
+    init(20, prob[0].l + prob[1].l);
+
     int max_index_q = compute_max_index(prob[1]); //Todo: besseren namen finden
     int max_index_p = compute_max_index(prob[0]);
 
@@ -576,3 +592,137 @@ int main (int argc, char ** argv)
 
     return 0;
 }
+
+
+// cache anfang
+
+int nr_of_cache_entries;
+int nr_of_elements;
+
+double** data;
+
+int* look_up_table; // translates data id to cache position
+int* reverse_look_up_table; // translates cache positions to id
+int* circular_array; // safes order in which cache pos was inserted
+
+int ca_first;
+int ca_last;
+int ca_free_pos; // safes which pos has no yet been occupied
+
+void init(int noce, int noe) {
+    nr_of_cache_entries = noce;
+    nr_of_elements = noe;
+
+    // allocate memory
+    look_up_table = malloc( sizeof(int) * nr_of_elements );
+    memset( look_up_table, -1, sizeof(int) * nr_of_elements );
+
+    reverse_look_up_table = malloc( sizeof(int) * nr_of_cache_entries );
+    memset( reverse_look_up_table, -1, sizeof(int) * nr_of_cache_entries );
+
+    circular_array = malloc( sizeof(int) * nr_of_cache_entries );
+    memset( circular_array, -1, sizeof(int) * nr_of_cache_entries );
+
+    data = malloc( sizeof( double* ) * nr_of_cache_entries);
+
+    int i;
+    for(i=0;i<nr_of_cache_entries; i++)
+    {
+        data[i] = malloc( sizeof( double) * (prob[0].l + prob[1].l) );
+    }
+
+    // init pointer
+    ca_first = 0;
+    ca_last = nr_of_cache_entries - 1;
+
+}
+
+void cleanup() {
+    free(reverse_look_up_table);
+    free(look_up_table);
+    free(circular_array);
+    int i;
+    for(i=0;i<nr_of_cache_entries; i++)
+    {
+        free( data[i] );
+    }
+    free(data);
+}
+
+void get_data(int id, int set, int pointer)
+{
+//    data[pointer] = (double) id * id;
+
+    int i;
+    for(i=0;i<prob[0].l;i++)
+    {
+        printf("set1 = %d, id = %d,  set2 = %d, id = %d res = %f\n", set, id, 0, i,  kernel(set, id, 0, i));
+        data[pointer][i] = kernel(set, id, 0, i);
+    }
+
+    for(i=0;i<prob[1].l;i++)
+    {
+        printf("set1 = %d, id = %d,  set2 = %d, id = %d   res = %f \n", set, id, 1, i,  kernel(set, id, 1, i));
+        data[pointer][i + prob[0].l] = kernel(set, id, 1, i);
+    }
+}
+
+
+void ca_add(int id) {
+    int last_id = reverse_look_up_table[ ca_last ]; // clean up look up table
+    if(last_id != -1)
+    {
+        //pos = look_up_table[ last_id ];
+        look_up_table[ last_id ] = -1;
+    } else {
+        circular_array[ca_last] = ca_free_pos;
+        ca_free_pos++;
+    }
+
+    //circular_array[ca_last] = pos;
+    ca_first = ca_last;
+    ca_last = ca_last - 1;
+    if(ca_last<0) ca_last = nr_of_cache_entries - 1;
+
+    reverse_look_up_table[ca_first] = id;
+    look_up_table[id] = ca_first;
+}
+
+void ca_bring_forward(int pos)
+{
+    int temp = circular_array[pos];
+    int pos_last = pos;
+    int pos_current = pos - 1;
+    if(pos_current < 0) pos_current = nr_of_cache_entries - 1;
+    while(pos_last != ca_first)
+    {
+        circular_array[pos_last] = circular_array[pos_current];
+
+        pos_last = pos_current;
+        pos_current--;
+        if(pos_current < 0) pos_current = nr_of_cache_entries - 1;
+    }
+
+    circular_array[ca_first] = temp;
+
+
+}
+
+double* get_element(int id, int set)
+{
+    printf(" get_element(): id = %d, set = %d \n", id, set);
+    int idset = id + set* prob[0].l;
+    if( look_up_table[idset] == -1 ) { // cache miss
+        ca_add(idset);
+        get_data(id, set, circular_array[ca_first]);
+        printf("cache miss, id = %d, set = %d\n", id, set);
+    } else { //cache hit
+        if(look_up_table[idset] != ca_first)
+        {
+            ca_bring_forward(look_up_table[idset]);
+        }
+    }
+    return data[circular_array[ca_first]];
+}
+
+// cache ende
